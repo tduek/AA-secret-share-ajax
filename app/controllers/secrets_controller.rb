423f2a82af550c0bb94fa5973bcc5ba834f25311
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-26 16:23:28 -0700

    add secret tag creation to controller

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -9,13 +9,26 @@ class SecretsController < ApplicationController
   end
 
   def create
+    tag_ids = params[:secret].delete(:tag_ids)
+
     @secret = Secret.new(params[:secret])
     @secret.author_id = current_user.id
+    ActiveRecord::Base.transaction do
+      @secret.save
+      @secret_tags = []
 
-    if @secret.save
-      redirect_to user_url(@secret.recipient_id)
-    else
-      render :new
+      tag_ids.each do |tag_id|
+        @secret_tags << SecretTagging.create(
+          :secret_id => @secret.id,
+          :tag_id => tag_id
+        )
+      end
+
+      if ([@secret] + @secret_tags).all? { |obj| obj.valid? }
+        redirect_to user_url(@secret.recipient_id)
+      else
+        render :new
+      end
     end
   end
 end
